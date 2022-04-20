9e5bcba90abdf2927129e3b07db5fa31fe43a2a3
hugo <loio.2601@gmail.com>
2022-04-20 22:48:48 +0100

    Multipole plot

--- app/multipole.cpp
+++ app/multipole.cpp
@@ -9,6 +9,24 @@
 using namespace std;
 using namespace arma;
 
+void scanQuadrupoleBBH2D(int nPoints, int nx, int ny, char* argv0, string name){
+  OData out(argv0, name);
+  double delta = 2/(double)nPoints;
+  vector<vector<double>> data;
+  vector<double> t1;
+  vector<double> quad;
+  BBH2D bbh2D(2,1);
+  for(int i = 0; i <= nPoints; i++){
+    t1.push_back(i*delta);
+    bbh2D.setIntraHop(i*delta);
+    quad.push_back(bbh2D.getQuadrupoleNested(nx,ny));
+  }
+  data.push_back(t1);
+  data.push_back(quad);
+  out.data(data);
+}
+
+
 int main (int arc, char ** argv) {
   cout << "\n2D BBH" << endl;
   double k1[2] = {0, M_PI/2};
@@ -37,6 +55,8 @@ int main (int arc, char ** argv) {
   bbh2D.setInterHop(0.2);
   cout << "Intercell hop: " << 0.2 << endl;
   cout << "Quadrupole moment nested: " << bbh2D.getQuadrupoleNested(30,30) << endl;
+
+  scanQuadrupoleBBH2D(100, 20, 20, argv[0], "QuadrupoleBBH2D_20_20.dat");
 
   return 0;
 }
---
+++ scripts/MultipoleGamma.py
@@ -0,0 +1,26 @@
+import helper as hp
+import matplotlib.pyplot as plt
+
+def MultipoleGamma(fname, show: bool, ylab):
+    data = hp.readfile(fname)
+
+    fig, ax = plt.subplots()
+    for i in range(1,len(data)):
+        ax.plot(data[0],data[i],'.')
+
+    ax.set(xlabel = r'$\frac{\gamma}{\lambda}$', ylabel = ylab)
+
+    fig.savefig(hp.plot_dir() + name + ".png", dpi = 200)
+    if(show):
+        plt.show()
+
+quadrupole = ["QuadrupoleBBH2D_20_20"] 
+
+octupole = []
+
+for name in quadrupole:
+    MultipoleGamma(name + ".dat", True, r'$q_{xy}$')
+
+for name in octupole:
+    MultipoleGamma(name + ".dat", True, r'$o_{xyz}$')
+
