1191c32a7c7d0553660ee056e30557d733987c11
hugo <loio.2601@gmail.com>
2022-07-04 00:30:10 +0100

    minor change

--- app/phaseDiagramSOTAI.cpp
+++ app/phaseDiagramSOTAI.cpp
@@ -5,66 +5,6 @@
 #include "OData.h"
 #include "MultiThread.h"
 
-//Multithreading solution
-void threadCalcQuad(int nAvg, int nThreads, int threadIndex, int * l, OData & out){
-  int nPoints = 21;
-  double m = 1.1;
-  int pointsPerThread = nPoints/nThreads;
-  int addPoints = 0;
-  if(nPoints % nThreads > threadIndex){
-    pointsPerThread++;
-  }
-  else{
-    addPoints = nPoints % nThreads;
-  }
-  int nStart = pointsPerThread*threadIndex + addPoints;
-  int nEnd = pointsPerThread*(threadIndex + 1) + addPoints;
-  double delta = 9/(double)(nPoints - 1);
-
-  vector<vector<double>> data;
-  vector<double> w;
-  vector<vector<double>> quad;
-  DisorderedSOTAI sotai(m);
-  sotai.setSize(l);
-
-  for(int e = 0; e < nAvg; e++){
-    quad.push_back(w);
-  }
-
-  for(int i = nStart; i < nEnd; i++){
-    w.push_back(i*delta);
-    sotai.setW(i*delta);
-    cout << i << " " << threadIndex << endl;
-    for(int e = 0; e < nAvg; e++){
-      sotai.generateDisorder();
-      quad[e].push_back(sotai.getQuadrupoleManyBody());
-    }
-  }
-
-  data.push_back(w);
-  for(int e = 0; e < nAvg; e++){
-    data.push_back(quad[e]);
-  }
-  out.data(data);
-}
-
-
-
-void scanQuadrupoleMulti(int nAvg, int * l, char * argv0, string name){
-  int nThreads = 8;
-  vector<thread> thrds;
-  OData out(argv0, name);
-
-  for(int i = 0; i < nThreads; i++){
-    thrds.push_back(thread(threadCalcQuad, nAvg, nThreads, i, l, ref(out)));
-  }
-
-  for(int i = 0; i < nThreads; i++){
-    thrds[i].join();
-  }
-
-}
-
 void threadQuad(int * l, double m, int nSamples, vector<double> & res, vector <double> params){
   DisorderedSOTAI sotai(m);
   sotai.setSize(l);
@@ -109,6 +49,11 @@ void quad2(vector<double> & res, vector<double> params){
   threadQuad(l, 1.1, 40, res, params);
 }
 
+void quad3(vector<double> & res, vector<double> params){
+  int l[2] = {40,40};
+  threadQuad(l, 1.1, 40, res, params);
+}
+
 void pol1(vector<double> & res, vector<double> params){
   int l[2] = {10,10};
   threadPol(l, 1.1, 40, res, params);
@@ -124,47 +69,57 @@ void pol3(vector<double> & res, vector<double> params){
   threadPol(l, 1.1, 40, res, params);
 }
 
+void pol4(vector<double> & res, vector<double> params){
+  int l[2] = {200,200};
+  threadPol(l, 1.1, 40, res, params);
+}
 
-int main (int arc, char ** argv) {
+
+int main (int argc, char ** argv) {
   //SOTAI
-  int l2D1[2] = {10,10};
-  int l2D2[2] = {20,20};
-  int l2D3[2] = {30,30};
-  cout << "Disordered SOTAI" << endl;
-  //scanQuadrupoleMulti(40, l2D1, argv[0], "phaseDiagramSOTAI_10x10_m1.1.dat");
-  //scanQuadrupoleMulti(40, l2D2, argv[0], "phaseDiagramSOTAI_20x20_m1.1.dat");
-  //scanQuadrupoleMulti(1, l2D3, argv[0], "phaseDiagramSOTAI_30x30_m1.1.dat");
+
+  int threadNumber = 8;
+  if(argc == 2){
+    threadNumber = stoi(argv[1]);
+  }
 
   vector<vector<double>> paramList;
-  int nPoints = 20;
-  for(int i = 0; i <= nPoints; i++){
+  int nPoints = 100;
+  for(int i = 60; i <= nPoints; i++){
     vector<double> param; 
     param.push_back(9*(double)i/(double)nPoints);
     paramList.push_back(param);
   }
 
   /*
-     MultiThread r1(quad1, paramList, 8);
+     MultiThread r1(quad1, paramList, threadNumber);
      r1.setFile(argv[0], "phaseDiagramSOTAI_10x10_m1.1.dat");
      r1.run();
 
-*/
-  MultiThread r2(quad2, paramList, 8);
-  r2.setFile(argv[0], "phaseDiagramSOTAI_20x20_m1.1.dat");
-  r2.run();
-  /*
-
-     MultiThread r3(pol1, paramList, 8);
+     MultiThread r3(pol1, paramList, threadNumber);
      r3.setFile(argv[0], "phaseDiagramSOTAIpol_10x10_m1.1.dat");
      r3.run();
 
-     MultiThread r4(pol2, paramList, 8);
+     MultiThread r2(quad2, paramList, threadNumber);
+     r2.setFile(argv[0], "phaseDiagramSOTAI_20x20_m1.1.dat");
+     r2.run();
+
+     MultiThread r4(pol2, paramList, threadNumber);
      r4.setFile(argv[0], "phaseDiagramSOTAIpol_50x50_m1.1.dat");
      r4.run();
-     */
 
-  MultiThread r5(pol3, paramList, 8);
-  r5.setFile(argv[0], "phaseDiagramSOTAIpol_100x100_m1.1.dat");
-  r5.run();
+     MultiThread r5(pol3, paramList, threadNumber);
+     r5.setFile(argv[0], "phaseDiagramSOTAIpol_100x100_m1.1.dat");
+     r5.run();
+
+     MultiThread r7(quad3, paramList, threadNumber);
+     r7.setFile(argv[0], "phaseDiagramSOTAI_40x40_m1.1.dat");
+     r7.run();
+
+*/
+
+  MultiThread r6(pol4, paramList, threadNumber);
+  r6.setFile(argv[0], "phaseDiagramSOTAIpol_200x200_m1.1.dat");
+  r6.run();
 
 }
--- scripts/PhaseDiagram.py
+++ scripts/PhaseDiagram.py
@@ -10,25 +10,26 @@ def plotPol():
     py = data[2:-1:2]
     p = 4*np.absolute(np.multiply(px,py))
 
-    ax.errorbar(data[0], np.average(px,axis=0), yerr = np.std(px,axis=0)/np.sqrt(np.size(data)), label = r'$p_x$, ' + labelsPol[i], capsize = 5, linestyle='-')
-    ax.errorbar(data[0], np.average(py,axis=0), yerr = np.std(py,axis=0)/np.sqrt(np.size(data)), label = r'$p_y$, ' + labelsPol[i], capsize = 5, linestyle='-')
-    ax.errorbar(data[0], np.average(p,axis=0), yerr = np.std(p,axis=0)/np.sqrt(np.size(data)), label = r'$P$, ' + labelsPol[i], capsize = 5, linestyle='-')
+    #ax.errorbar(data[0], np.average(px,axis=0), yerr = np.std(px,axis=0)/np.sqrt(np.size(data)), label = r'$p_x$, ' + labelsPol[i], capsize = 5, linestyle='-')
+    #ax.errorbar(data[0], np.average(px,axis=0), yerr = np.std(px,axis=0)/np.sqrt(np.size(data)), label = r'$p_x$, ' + labelsPol[i], capsize = 5, linestyle='-')
+    #ax.errorbar(data[0], np.average(py,axis=0), yerr = np.std(py,axis=0)/np.sqrt(np.size(data)), label = r'$p_y$, ' + labelsPol[i], capsize = 5, linestyle='-')
+    ax.errorbar(data[0], np.average(p,axis=0), label = r'$P$, ' + labelsPol[i], capsize = 5, linestyle='-')
 
 def plotQuad():
     data = hp.readfile(namesQuad[i] + ".dat")
 
     data = data[:, data[0,:].argsort()]
-    ax.errorbar(data[0], np.average(data[1:],axis=0), yerr = np.std(data[1:],axis=0)/np.sqrt(np.size(data)), label = r'$q_{xy}$, ' + labelsQuad[i], capsize = 5, linestyle='-')
+    ax.errorbar(data[0], np.average(data[1:],axis=0), label = r'$q_{xy}$, ' + labelsQuad[i], capsize = 5, linestyle='-')
 
 
 plot_name = "phaseDiagramSOTAI"
-namesQuad = ["phaseDiagramSOTAI_10x10_m1.1", "phaseDiagramSOTAI_20x20_m1.1"]
-#namesQuad = ["phaseDiagramSOTAI_10x10_m1.1"]
-labelsQuad = ["L = 10", "L = 20"]
+#namesQuad = ["phaseDiagramSOTAI_10x10_m1.1", "phaseDiagramSOTAI_20x20_m1.1"]
+namesQuad = ["phaseDiagramSOTAI_20x20_m1.1"]
+labelsQuad = ["L = 20"]
 #namesPol = ["phaseDiagramSOTAIpol_10x10_m1.1", "phaseDiagramSOTAIpol_50x50_m1.1"]
 #labelsPol = ["L = 10", "L = 50"]
-namesPol = ["phaseDiagramSOTAIpol_50x50_m1.1","phaseDiagramSOTAIpol_100x100_m1.1"]
-labelsPol = ["L = 50", "L = 100"]
+namesPol = ["phaseDiagramSOTAIpol_100x100_m1.1"]
+labelsPol = ["L = 100"]
 
 
 fig, ax = plt.subplots()
